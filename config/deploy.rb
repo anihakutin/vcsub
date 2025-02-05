57fdfbb480b2855e5cee6346156a41d841ca153e
Heshie <4hbrody@gmail.com>
2025-02-05 12:04:16 -0500

    fix mainfest

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,15 @@ namespace :deploy do
     desc 'Create asset directories'
     task :create_dirs do
       on roles(:web) do
-        execute "mkdir -p #{release_path}/app/assets/builds"
+        execute "mkdir -p #{shared_path}/app/assets/builds"
+        execute "ln -s #{shared_path}/app/assets/builds #{release_path}/app/assets/builds"
         execute "mkdir -p #{release_path}/app/assets/images"
       end
     end
   end
 end
+
+# Ensure the builds directory exists and is shared between deployments
+append :linked_dirs, 'app/assets/builds'
 
 before 'deploy:assets:precompile', 'deploy:assets:create_dirs' 
