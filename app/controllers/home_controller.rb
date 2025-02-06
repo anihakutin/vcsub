9d2437edb49254aba971cbccd0ccb8afdf6a7cab
Heshie <4hbrody@gmail.com>
2025-02-05 23:55:08 -0500

    limit home listings to 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @listings = Listing.order(created_at: :desc).limit(6)
-    @requests = Request.order(created_at: :desc).limit(6)
+    @listings = Listing.order(created_at: :desc).limit(3)
+    @requests = Request.order(created_at: :desc).limit(3)
   end
 end 
