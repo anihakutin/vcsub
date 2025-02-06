fb33ccb0b51f518e7d7567a037ab28463ff7b944
Heshie <4hbrody@gmail.com>
2025-02-06 00:16:31 -0500

    fix logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :check_profile_completion
+  before_action :check_profile_completion, unless: -> { params[:action] == 'destroy' }
 
   protected
 
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   private
 
   def check_profile_completion
-    if user_signed_in? && current_user.sign_in_count == 1 && 
+    if user_signed_in? && 
        !current_user.profile_completed? && 
        controller_name != 'profiles'
       redirect_to edit_profile_path, notice: "Please complete your profile first"
