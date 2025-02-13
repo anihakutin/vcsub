f5d868fc5fc694b92b9e02daf0640240f019449f
Heshie <4hbrody@gmail.com>
2025-02-13 16:25:49 -0500

    add fav icon

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 //= link_tree ../builds
 //= link application.css
-//= link application.js 
+//= link application.js
+//= link favicon.ico
+//= link favicon-16x16.png
+//= link favicon-32x32.png
+//= link apple-touch-icon.png 
